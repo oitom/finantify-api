8e7e7b82c239ab41cf65753c7028c0eeef6c6001
Wellington Costa <wcosta.ale@gmail.com>
2024-01-20 22:25:33 -0300

    Refresh token jwt implementado

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Post, Req, UseGuards } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
+import { Request } from "express";
 
 @Controller("auth")
 export class AuthController {
@@ -10,5 +17,16 @@ export class AuthController {
   @Post("login")
   async login(@Req() req: any) {
     return await this.authService.login(req.user);
+  }
+
+  @Post("refresh-token")
+  async refreshToken(@Req() req: Request) {
+    const refreshToken = req.headers.authorization?.split(" ")[1];
+
+    if (!refreshToken) {
+      throw new UnauthorizedException("Refresh token missing");
+    }
+
+    return this.authService.refreshTokens(refreshToken);
   }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersService } from "../users/users.service";
 import * as bcrypt from "bcrypt";
 import { User } from "../users/entities/users.entity";
@@ -31,6 +31,43 @@ export class AuthService {
 
     return {
       token: this.jwtService.sign(payload),
+      refresh_token: this.generateRefreshToken(user),
     };
+  }
+
+  async refreshTokens(refreshToken: string) {
+    const decoded = this.jwtService.decode(refreshToken) as { sub: string };
+
+    if (!decoded || !decoded.sub) {
+      throw new UnauthorizedException("Invalid refresh token");
+    }
+
+    const user = await this.usersService.findOne(decoded.sub);
+    if (!user) {
+      throw new UnauthorizedException("User not found");
+    }
+
+    const accessToken = this.jwtService.sign({
+      sub: user.id,
+      email: user.email,
+    });
+
+    return {
+      token: accessToken,
+      refresh_token: this.generateRefreshToken(user),
+    };
+  }
+
+  async verifyToken(token: string): Promise<User | null> {
+    try {
+      const decoded = this.jwtService.verify(token) as { sub: string };
+      return this.usersService.findOne(decoded.sub);
+    } catch (error) {
+      return null;
+    }
+  }
+
+  private generateRefreshToken(user: User) {
+    return this.jwtService.sign({ sub: user.id }, { expiresIn: "7d" });
   }
 }
